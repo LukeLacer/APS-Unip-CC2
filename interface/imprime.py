0a01bc2c64665aea01569453e9812222fa849cb7
Lucas Lacerda de Lima <lucas_lacer@hotmail.com>
2020-11-04 12:55:29 -0300

    teste de imprime retirado

--- interface/imprime.py
+++ interface/imprime.py
@@ -29,5 +29,3 @@ def texto(texto):
     #imprime o texto
     for i in resultado:
         print('│ ' + i + ' ' * (98 - len(i)) + ' │')
-
-texto('No Brasil, 98% de todas as latas de alumínio vão parar na indústria vindo da coleta de reciclagem, custando 5% do valor de fabricação de uma lata advinda da extração da natureza. Aço, alumínio, cobre, estanho e zinco são os principais metais recicláveis, porém é importante falar que clipes e grampos, esponjas e palhas de aço, latas de aerossóis, latas de tintas, vernizes e solventes, latas de produtos tóxicos como inseticidas e pesticidas, produtos feitos com metais pesados, inclusive pilhas, baterias e tomadas não sao recicláveis. Pilhas, bateriais e outros produtos com metal pesado devem ainda ser descartados em pontos de coelta especiais.')
