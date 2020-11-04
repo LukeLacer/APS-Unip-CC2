b37ab190a2e0a6360aa1b77af5c640f32e7775c0
Lucas Lacerda de Lima <lucas_lacer@hotmail.com>
2020-11-04 13:09:59 -0300

    interface adicionada a exibição de reciclavel

--- interface/json_scripts.py
+++ interface/json_scripts.py
@@ -1,5 +1,5 @@
 import json #modulo que gerencia o json
-import imprime
+import imprime, moldura
 
 #pega o json e joga no dicionario dados_reciclaveis
 with open('data/dados_reciclaveis.json', encoding="utf8") as f:
@@ -8,20 +8,20 @@ with open('data/dados_reciclaveis.json', encoding="utf8") as f:
 #percorre o dicionario imprimindo TODOS os dados
 def reciclaveis(data):
     for p in data['reciclaveis']:
-        print(p['material'].upper())
-        print('Tereciclaveismpo de Degradação: ' + p['tempo_degradacao'])
-        print('')
+        moldura.default_superior()
+        imprime.texto(p['material'].upper())
+        imprime.texto('Tempo de Degradação: ' + p['tempo_degradacao'])
+        imprime.texto('')
         imprime.texto(p['resumo'])
-        print('')
-        print('PODEMOS RECICLAR:')
+        imprime.texto('')
+        imprime.texto('PODEMOS RECICLAR:')
         for i in p['reciclaveis']:
-            print('✔  ' + i)
-        print('')
-        print('NÃO PODEMOS RECICLAR:')
+            imprime.texto('✔  ' + i)
+        imprime.texto('')
+        imprime.texto('NÃO PODEMOS RECICLAR:')
         for i in p['nao_reciclaveis']:
-            print('✘  ' + i)
-        print('')
-        print('------------------------------------------------------------------------------------')
+            imprime.texto('✘  ' + i)
+        moldura.default_inferior()
         print('')
 #fim_reciclaveis
 
